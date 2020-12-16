f50a92aab1e0c1ff4ff943c1585c7d1ba2b6ef11
tetrisranner@gmail.com <ec2-user@ip-172-31-0-14.ec2.internal>
2020-12-16 11:56:25 +0000

    [finish]task2

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  get 'homes/apptop'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- get '/' => 'homes#apptop'
- post 'lists' => 'lists#create'
- get 'lists' => 'lists#index'
- get 'lists/:id' => 'lists#show', as: 'list'
- get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
- patch 'lists/:id' => 'lists#update', as: 'update_list'
- delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
+ root :to => 'homes#apptop'
+ resources :books
 end
